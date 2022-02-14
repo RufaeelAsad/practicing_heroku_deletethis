ec1bd63923c89dd14babf7ab03f6ef393b5e9976
Rufaeel Asad <rufaeelasad@gmail.com>
2022-02-14 20:32:30 +0500

    practicing

---
+++ index.mjs
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+const PORT = process.env.PORT;
+con
+
+app.get('/', (req, res) => {
+  res.send('ABI chek kr looo')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port AT http//:localhost:${PORT}`)
+})
